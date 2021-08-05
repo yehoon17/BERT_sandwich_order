e6943be4b1f8b229ad2a0ce7d881359ba1a3f676
JiChangHun <87739892+JiChangHun@users.noreply.github.com>
2021-08-05 10:39:55 +0900

    Update data_merge.py

--- data/generate/data_merge.py
+++ data/generate/data_merge.py
@@ -3,6 +3,7 @@ import src.data_generate_jungmin as jm
 import src.data_generate_jongrin as jr
 import src.data_generate_taehyung as th
 import src.data_generate_jihun as jh
+import src.data_generate_changhun as ch
 
 
 data = []
@@ -12,6 +13,7 @@ data.extend(jm.gen_data())
 data.extend(jr.gen_data())
 data.extend(th.gen_data())
 data.extend(jh.gen_data())
+data.extend(ch.gen_data())
 
 print(len(data))
 print(data[:5])
