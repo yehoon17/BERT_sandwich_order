ef7828ef7eabe9e1a78a8b17a20f51e1242183f6
JJulme <87740086+JJulme@users.noreply.github.com>
2021-08-05 10:29:08 +0900

    Update data_merge.py

--- data/generate/data_merge.py
+++ data/generate/data_merge.py
@@ -1,8 +1,10 @@
 import src.data_generate_yehoon as yh
+import src.data_generate_jungmin as jm
 
 data = []
 
 data.extend(yh.gen_data())
+data.extend(jm.gen_data())
 
 print(len(data))
 print(data[:5])
