d6ce1d30f9f006dfc334ec9d3a3a89d310693c33
khan <k151202@naver.com>
2021-08-10 12:55:14 +0900

    gitignore and main

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import tensorflow
+
 assert tensorflow.__version__ == "1.15"
-import src.tokenizationK 
-import src.seq as seq
+from data.seq.src.seq import Seq
+from data.seq.resources.tokenizer import tokenizationK
+
 
 def main():
     seq_in = []
     seq_out = []
 
-    tokenizer = src.tokenizationK.FullTokenizer(vocab_file = "data\seq\resources\vocab.korean.rawtext.list")
+    tokenizer = tokenizationK.FullTokenizer(
+        vocab_file="data/seq/resources/tokenizer/vocab.korean.rawtext.list"
+    )
 
     with open("data/seq/resources/data.txt", "r", encoding="utf-8") as f:
         for sentence in f.readlines():
-            s = seq.Seq(sentence, tokenizer)
+            s = Seq(sentence, tokenizer)
             seq_in.append(s.get_seq_in())
             seq_out.append(s.get_seq_out())
 
-    
     with open("data/seq/resources/seq_in.txt", "w", encoding="utf-8") as f:
         for line in seq_in:
-            f.write(line+"\n")
+            f.write(line + "\n")
 
     with open("data/seq/resources/seq_out.txt", "w", encoding="utf-8") as f:
         for line in seq_out:
-            f.write(line+"\n")
-    
+            f.write(line + "\n")
 
 
 if __name__ == "__main__":
