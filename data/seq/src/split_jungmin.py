2f6778f08e1ae91e94b3c8837a951aebee8ae4a1
JJulme <87740086+JJulme@users.noreply.github.com>
2021-08-10 12:01:51 +0900

    Update split_jungmin.py

--- data/seq/src/split_jungmin.py
+++ data/seq/src/split_jungmin.py
@@ -11,6 +11,6 @@ def split(seq_in, seq_out, random_state=None):
         X_test_20, y_test_20, test_size=0.5, random_state=random_state)
     
     # 리턴값: X_훈련, y_훈련, X_테스트, y_테스트, X_검증, y_검증
-    return X_train, y_train, X_test, y_test, X_vaild, y_valid
+    return X_train, y_train, X_test, y_test, X_valid, y_valid
 
-# X_train, y_train, X_test, y_test, X_vaild, y_valid = split(seq_in, seq_out, 42)
+# X_train, y_train, X_test, y_test, X_valid, y_valid = split(seq_in, seq_out, 42)
