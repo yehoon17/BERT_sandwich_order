f04c49348e7378cf6aa4cac9de6a918d4c7c0f26
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-08-13 08:56:10 +0900

    Update utils.py (#39)
    bert_to_array.py 의 BERTToArray.__to_array() 함수 split()을 하기 때문에 여기의 split()을 제거

--- bert_slot_kor/utils.py
+++ bert_slot_kor/utils.py
@@ -19,11 +19,11 @@ class Reader:
         # (텍스트 파일의 한 라인이 리스트의 한 요소가 되도록)
         with open(os.path.join(dataset_folder_path,"seq_in.txt"), "r") as f:
             for line in f.readlines():
-                text_arr.append(line.split())
+                text_arr.append(line)
 
         with open(os.path.join(dataset_folder_path,"seq_out.txt"), "r") as f:
             for line in f.readlines():
-                tags_arr.append(line.split())
+                tags_arr.append(line)
 
         assert len(text_arr) == len(tags_arr)
         return text_arr, tags_arr
