038a7fd8ef2ab32e956290333421c322734f5625
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-08-12 11:29:27 +0900

    Update inference.py (#30)

--- bert_slot_kor/inference.py
+++ bert_slot_kor/inference.py
@@ -4,9 +4,10 @@ import os
 import pickle
 import argparse
 
-############################### TODO ##########################################
-# 필요한 모듈 불러오기
-###############################################################################
+# 필요한 모듈 불러오기 
+from to_array.bert_to_array import BERTToArray
+from to_array.tokenizationK import FullTokenizer
+from models.bert_slot_model import BertSlotModel
 
 import tensorflow as tf
 
@@ -36,9 +37,23 @@ if __name__ == "__main__":
     bert_model_hub_path = "/content/drive/MyDrive/bert-module"
     ####################################################################
 
-    ############################### TODO ###############################
     # 모델과 기타 필요한 것들 불러오기
-    ####################################################################
+    
+    vocab_file = os.path.join(bert_model_hub_path,
+                              "assets/vocab.korean.rawtext.list")
+    bert_to_array = BERTToArray(vocab_file)
+    
+    # loading models
+    print("Loading models ...")
+    if not os.path.exists(load_folder_path):
+        print("Folder `%s` not exist" % load_folder_path)
+    
+    tags_to_array_path = os.path.join(load_folder_path, "tags_to_array.pkl")
+    with open(tags_to_array_path, "rb") as handle:
+        tags_to_array = pickle.load(handle)
+        slots_num = len(tags_to_array.label_encoder.classes_)
+        
+    model = BertSlotModel.load(load_folder_path, sess)
     
     
     while True:
@@ -51,9 +66,23 @@ if __name__ == "__main__":
         if input_text == "quit":
             break
     
-    ############################### TODO ###############################
     # 사용자가 입력한 한 문장을 슬롯태깅 모델에 넣어서 결과 뽑아내기
-    ####################################################################
+    ################# TODO vocab_file 경로 채우기 ###########################
+    tokenizer = FullTokenizer(vocab_file="")
+    ########################################################################
+    text_arr = tokenizer.tokenize(input_text)
+
+    input_ids, input_mask, segment_ids = bert_to_array.transform(text_arr)
     
+    # predict slots
+    inferred_tags, slots_score = model.predict_slots([input_ids,
+                                                      input_mask,
+                                                      segment_ids],
+                                                     tags_to_array)
+
+    # 예측된 슬롯 출력
+    print(inferred_tags)
+
+   
     tf.compat.v1.reset_default_graph()
     
