af4097a3a980ea199d4196cb79c65b4852a86a5b
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-08-12 10:14:07 +0900

    Update utils.py (#25)

--- bert_slot_kor/utils.py
+++ bert_slot_kor/utils.py
@@ -15,10 +15,15 @@ class Reader:
         text_arr = []
         tags_arr = []
         
-        ########################### TODO ###############################
         # seq.in과 seq.out을 읽어들여서 리스트로 만들기
         # (텍스트 파일의 한 라인이 리스트의 한 요소가 되도록)
-        ################################################################
+        with open(os.path.join(dataset_folder_path,"seq_in.txt"), "r") as f:
+            for line in f.readlines():
+                text_arr.append(line.split())
+
+        with open(os.path.join(dataset_folder_path,"seq_out.txt"), "r") as f:
+            for line in f.readlines():
+                text_arr.append(line.split())
 
         assert len(text_arr) == len(tags_arr)
         return text_arr, tags_arr
