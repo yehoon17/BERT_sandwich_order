b5422fe910c4f63c48718be41830dc1598688f40
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-08-17 10:58:43 +0900

    Update data_to_seq.py (#50)

--- data/seq/data_to_seq.py
+++ data/seq/data_to_seq.py
@@ -2,6 +2,8 @@ import tensorflow
 assert tensorflow.__version__ == "1.15"
 import src.tokenizationK 
 import src.seq as seq
+import src.split
+import pandas as pd
 
 def main():
     seq_in = []
@@ -24,8 +26,37 @@ def main():
     with open("data/seq/resources/seq_out.txt", "w", encoding="utf-8") as f:
         for line in seq_out:
             f.write(line+"\n")
-    
 
+    # 데이터 train, test, valid set 으로 나누기
+    spliter = split.Spliter(pd.Series(seq_in), pd.Series(seq_out))
+    spliter.split()
+    
+    X_train, y_train = spliter.get_train()
+    with open("data/seq/resources/train_seq_in.txt", "w", encoding="utf-8") as f:
+      for line in X_train:
+        f.write(" ".join(list(map(str,line)))+"\n")
+
+    with open("data/seq/resources/train_seq_out.txt", "w", encoding="utf-8") as f:
+      for line in y_train:
+        f.write(" ".join(list(map(str,line)))+"\n")
+
+    X_test, y_test = spliter.get_test()
+    with open("data/seq/resources/test_seq_in.txt", "w", encoding="utf-8") as f:
+      for line in X_test:
+        f.write(" ".join(list(map(str,line)))+"\n")
+
+    with open("data/seq/resources/test_seq_out.txt", "w", encoding="utf-8") as f:
+      for line in y_test:
+        f.write(" ".join(list(map(str,line)))+"\n")
+
+    X_valid, y_valid = spliter.get_valid()
+    with open("data/seq/resources/valid_seq_in.txt", "w", encoding="utf-8") as f:
+      for line in X_valid:
+        f.write(" ".join(list(map(str,line)))+"\n")
+
+    with open("data/seq/resources/valid_seq_out.txt", "w", encoding="utf-8") as f:
+      for line in y_valid:
+        f.write(" ".join(list(map(str,line)))+"\n")
 
 if __name__ == "__main__":
     main()
