f2cbf9aca9caa325a22a6d2df985787c2c85e478
yehoon17 <83195606+yehoon17@users.noreply.github.com>
2021-08-19 11:58:32 +0900

    Update main.py (#53)
    * Update main.py
    
    * Update main.py

--- web_demo/app/main.py
+++ web_demo/app/main.py
@@ -56,17 +56,23 @@ app.static_folder = "static"
 def home():
     # 슬롯 사전 만들기
     app.slot_dict = {
-        "sandwich": None,
-        "length": None,
-        "bread": None,
-        "cheese": None,
-        "sauce": None,
-        "vegetable": None,
+        "sandwich": [],
+        "length": [],
+        "bread": [],
+        "cheese": [],
+        "sauce": [],
+        "vegetable": [],
         # "topping": None,
     }
     # 점수제한
     app.score_limit = 0.8
-
+    
+    # 변수 
+    app.ask_veg = False
+    app.confirm_veg = False
+    app.ask_sauce = False
+    app.confirm_sauce = False
+    
     return render_template("index.html")
 
 
@@ -91,13 +97,7 @@ def get_bot_response():
     print("slots_score:", slots_score[0])
 
     # 슬롯에 해당하는 텍스트를 담을 변수 설정
-    sandwich_text = ""
-    length_text = ""
-    bread_text = ""
-    cheese_text = ""
-    sauce_text = ""
-    vegetable_text = ""
-    # topping_text = ""
+    slot_text = {k:"" for k in app.slot_dict}
 
     # 메뉴 및 선택지
     sandwich = [
@@ -158,55 +158,46 @@ def get_bot_response():
     # 슬롯태깅 실시
     for i in range(0, len(inferred_tags[0])):
         if slots_score[0][i] >= app.score_limit:
-            if inferred_tags[0][i] == "sandwich":
-                sandwich_text += text_arr[i]
-                app.slot_dict["sandwich"] = re.sub("_", "", sandwich_text)
-
-            elif inferred_tags[0][i] == "length":
-                length_text += text_arr[i]
-                app.slot_dict["length"] = re.sub("_", "", length_text)
-
-            elif inferred_tags[0][i] == "bread":
-                bread_text += text_arr[i]
-                app.slot_dict["bread"] = re.sub("_", "", bread_text)
-
-            elif inferred_tags[0][i] == "cheese":
-                cheese_text += text_arr[i]
-                app.slot_dict["cheese"] = re.sub("_", "", cheese_text)
-
-            elif inferred_tags[0][i] == "sauce":
-                sauce_text += text_arr[i]
-                app.slot_dict["sauce"] = re.sub("_", "", sauce_text)
-
-            elif inferred_tags[0][i] == "vegetable":
-                vegetable_text += text_arr[i]
-                app.slot_dict["vegetable"] = re.sub("_", "", vegetable_text)
-
-            # elif inferred_tags[0][i] == "topping":
-            # topping_text += text_arr[i]
-            # app.slot_dict["topping"] = re.sub("_", "", topping_text)
+            if not inferred_tags[0][i] == "0":
+                slot_text[inferred_tags[0][i]] += re.sub("_", " ", text_arr[i])
         else:
             print("something went wrong!")
 
+    #print(slot_text)
     # 메뉴판의 이름과 일치하는지 검증
-    for k, v in app.slot_dict.items():
-        try:
-            answer = difflib.get_close_matches(
-                app.slot_dict[k], locals()[k], cutoff=0.5
-            )
-            if answer:
-                app.slot_dict[k] = answer
-            else:
-                app.slot_dict[k] = None
-        except:
-            app.slot_dict[k] = None
+    for k in app.slot_dict:
+      #print("k:",k)
+      for x in locals()[k]:
+        #print(x, slot_text[k])
+        m = re.search(x, slot_text[k])
+        if m:
+          app.slot_dict[k].append(m.group())
+
 
     print(app.slot_dict)
     # 슬롯이 채워지지 않았을때 체크
     # empty_slot -> 비어있는 메뉴의 리스트
     empty_slot = [menu[k] for k, v in app.slot_dict.items() if app.slot_dict[k] == None]
 
-    if empty_slot:
+    # 채소 슬롯이 비었을 때
+    if "제외할 채소" in empty_slot:
+      if not app.ask_veg:
+        message = "안 드시는 채소를 선택해주세요."
+        app.ask_veg = True
+      else:
+        if not app.confirm_veg:
+          message = "선택한 채소가 없습니다. 채소는 다 넣어드릴까요?\n(예/아니오)"
+          app.confirm_veg = True
+        else:
+          if userText.strip() == "예":
+              message = f"""
+              채소는 다 넣어드리겠습니다.
+              {", ".join(empty_slot)} + "가 아직 선택되지 않았습니다.
+              """
+          elif userText.strip() == "아니오":
+            app.ask_veg = False
+            app.confirm_veg = False
+    elif empty_slot:
         message = ", ".join(empty_slot) + "가 아직 선택되지 않았습니다."
     else:
         # 빈 슬롯이 없을때
@@ -216,13 +207,15 @@ def get_bot_response():
             message = "알겠습니다. 다시 주문해주세요."
             # 재주문을 위해 슬롯 초기화
             app.slot_dict = {
-                "sandwich": None,
-                "length": None,
-                "bread": None,
-                "cheese": None,
-                "sauce": None,
-                "vegetable": None,
+                "sandwich": [],
+                "length": [],
+                "bread": [],
+                "cheese": [],
+                "sauce": [],
+                "vegetable": [],
             }
+            app.ask_veg = False
+            app.confirm_veg = False
         else:
             order = []
             for k, v in app.slot_dict.items():
